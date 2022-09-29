74732e96d745ec1b9657fb4d719e49fd468a9811
max <matrixbyine.gmail.com>
2022-09-29 21:32:11 +0300

    Создали метод для вывода элемента длиной не более 3 символов

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,1 +1,12 @@
 ﻿string[] array = { "-2", "Monday", "123", "Wednes day" };
+
+void ArrayStr(string[] masstring)
+{
+   Console.Write("[");
+   for (int i = 0; i < array.Length; i++)
+      if (array[i].Length >= 3)
+      {
+         if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+         else Console.WriteLine($"{array[i]}]");
+      }
+}
